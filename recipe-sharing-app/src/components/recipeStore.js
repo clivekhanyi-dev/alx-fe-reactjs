8fb89b8aadcf3f5cadac72a56c563a40d60800e8
clivekhanyi <Clivekhanyi@gmail.com>
2026-01-25 19:22:32 +0200

    Add both addRecipe and setRecipes to satisfy checker

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,8 +2,11 @@ import { create } from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
+
   addRecipe: (recipe) =>
     set((state) => ({
       recipes: [...state.recipes, recipe],
     })),
+
+  setRecipes: (recipes) => set({ recipes }),
 }));
